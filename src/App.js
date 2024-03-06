5183fcfffc6dcf72015f743056554f2ce749f6c0
EC2 Default User <ec2-user@ip-172-31-30-101.eu-north-1.compute.internal>
2024-03-06 16:04:24 +0000

    Adding route to deply animal details by id

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ const appRouter = createBrowserRouter(createRoutesFromElements(
 <Route path='/' element={<Root />} >
   <Route index element={<HomePage /> } />
   <Route path=':type' element={ <HomePage /> }/> 
+  <Route path=':type/:id' element={ <PetDetailsPage /> } />
 </Route >
 ));
 
