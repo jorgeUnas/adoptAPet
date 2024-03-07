380fd8eb4be1274bb2d89b89374a9df3ea128173
EC2 Default User <ec2-user@ip-172-31-30-101.eu-north-1.compute.internal>
2024-03-07 10:18:00 +0000

    implementing a navigation bar utility with useNavigation and deploying SearchPage component

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import { createBrowserRouter, RouterProvider, createRoutesFromElements, Route } 
 // create router with JSX Route elements
 const appRouter = createBrowserRouter(createRoutesFromElements(
 <Route path='/' element={<Root />} >
+  <Route path='search' element={ <SearchPage /> }/> 
   <Route index element={<HomePage /> } />
   <Route path=':type' element={ <HomePage /> }/> 
   <Route path=':type/:id' element={ <PetDetailsPage /> } />
